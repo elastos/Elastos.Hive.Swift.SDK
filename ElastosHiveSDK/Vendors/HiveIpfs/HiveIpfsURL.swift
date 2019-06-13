4426bb1802d200ece888488c776eb36d2abe310e
liaihong <478701610@qq.com>
2019-06-13 19:16:13 +0800

    fix verify

--- ElastosHiveSDK/Vendors/HiveIpfs/HiveIpfsURL.swift
+++ ElastosHiveSDK/Vendors/HiveIpfs/HiveIpfsURL.swift
@@ -21,84 +21,103 @@ enum HIVE_SUB_Url: String {
 
 let KEYCHAIN_IPFS_UID  = "uid"
 class HiveIpfsURL {
-   static var URL_POOL = [
-                    "http://52.83.159.189:9095/api/v0/",
-                    "http://52.83.119.110:9095/api/v0/",
-                    "http://3.16.202.140:9095/api/v0/",
-                    "http://18.217.147.205:9095/api/v0/",
-                    "http://18.219.53.133:9095/api/v0/"]
+    static var URL_POOL = [
+        "http://52.83.159.189:9095/api/v0/",
+        "http://52.83.119.110:9095/api/v0/",
+        "http://3.16.202.140:9095/api/v0/",
+        "http://18.217.147.205:9095/api/v0/",
+        "http://18.219.53.133:9095/api/v0/"]
 
     internal static let IPFS_NODE_API_BASE = HiveIpfsURL.URL_POOL[0]
     internal static let IPFS_NODE_API_VRESION = "http://52.83.159.189:9095/version"
 
     class func validURL() -> HivePromise<Bool> {
         let promise = HivePromise<Bool> { resolver in
-
-            let validId = URL_POOL.filter({ (ip) -> Bool in
-                let uid = HelperMethods.getKeychain(KEYCHAIN_IPFS_UID, .IPFSACCOUNT) ?? ""
-                if uid == "" {
-                    return (validUseNewUID(ip).result != nil)
-                }else {
-                    return (validUseStat(ip, uid).result != nil)
-                }
-            }).first
-
-            if validId != nil {
-                URL_POOL =  URL_POOL.sorted { (first, last) -> Bool in
-                    if validId == first {
-                        return true
+            let uid = HelperMethods.getKeychain(KEYCHAIN_IPFS_UID, .IPFSACCOUNT) ?? ""
+            if uid == "" {
+                validUseNewUID(IPFS_NODE_API_BASE, { (url) in
+                    if url == "" {
+                        resolver.reject(HiveError.failue(des: "ALL URLS ARE INVALID"))
+                    }else {
+                        URL_POOL =  URL_POOL.sorted { (first, last) -> Bool in
+                            if url == first {
+                                return true
+                            }
+                            return false
+                        }
+                        resolver.fulfill(true)
                     }
-                    return false
-                }
-                resolver.fulfill(true)
+                })
             }
             else {
-                let error = HiveError.failue(des: "ALL URLS ARE INVALID")
-                print(error.localizedDescription)
-                resolver.reject(error)
-            }
-        }
-        return promise
-    }
-
-    class func validUseNewUID(_ ip: String) -> HivePromise<Bool> {
-        let promise = HivePromise<Bool> { resolver in
-            let url = ip + HIVE_SUB_Url.IPFS_UID_NEW.rawValue
-            Alamofire.request(url,
-                              method: .post,
-                              parameters: nil,
-                              encoding: JSONEncoding.default,
-                              headers: nil)
-                .responseJSON { (dataResponse) in
-                    guard dataResponse.response?.statusCode == 200 else{
-                        resolver.fulfill(false)
-                        return
+                validUseStat(IPFS_NODE_API_BASE, uid, { (url) in
+                    if url == "" {
+                        resolver.reject(HiveError.failue(des: "ALL URLS ARE INVALID"))
+                    }else {
+                        guard url == URL_POOL.first else{
+                            URL_POOL =  URL_POOL.sorted { (first, last) -> Bool in
+                                if url == first {
+                                    return true
+                                }
+                                return false
+                            }
+                            resolver.fulfill(true)
+                            return
+                        }
+                        resolver.fulfill(true)
                     }
-                    resolver.fulfill(true)
-            }
-        }
-        return promise
-    }
-
-    class func validUseStat(_ ip: String, _ uid: String) -> HivePromise<Bool> {
-        let promise = HivePromise<Bool> { resolver in
-            let url = ip + HIVE_SUB_Url.IPFS_FILES_STAT.rawValue
-            let param = ["uid": uid,"path": "/"]
-            Alamofire.request(url,
-                              method: .post,
-                              parameters: param,
-                              encoding: URLEncoding.queryString,
-                              headers: nil)
-                .responseJSON(completionHandler: { (dataResponse) in
-                    guard dataResponse.response?.statusCode == 200 else{
-                        resolver.fulfill(false)
-                        return
-                    }
-                    resolver.fulfill(true)
                 })
+            }
         }
         return promise
     }
+
+    class func validUseNewUID(_ url: String, _ result: @escaping (_ validUrl: String) -> Void) {
+        var i = 0
+        var currentUrl = url
+        let fullUrl = currentUrl + HIVE_SUB_Url.IPFS_UID_NEW.rawValue
+        Alamofire.request(fullUrl,
+                          method: .post,
+                          parameters: nil,
+                          encoding: JSONEncoding.default,
+                          headers: nil)
+            .responseJSON { (dataResponse) in
+                if dataResponse.response?.statusCode != 200 {
+                    i += 1
+                    if i > URL_POOL.count - 1 {
+                        currentUrl = ""
+                        return
+                    }
+                    validUseNewUID(URL_POOL[i], result)
+                }
+                result(currentUrl)
+        }
+    }
+
+    class func validUseStat(_ url: String, _ uid: String, _ result: @escaping (_ validUrl: String) -> Void) {
+
+        var i = 0
+        var currentUrl = url
+        let fullUrl = currentUrl + HIVE_SUB_Url.IPFS_FILES_STAT.rawValue
+        let param = ["uid": uid,"path": "/"]
+        Alamofire.request(fullUrl,
+                          method: .post,
+                          parameters: param,
+                          encoding: URLEncoding.queryString,
+                          headers: nil)
+            .responseJSON(completionHandler: { (dataResponse) in
+                if dataResponse.response?.statusCode != 200 {
+                    i += 1
+                    if i > URL_POOL.count - 1 {
+                        currentUrl = ""
+                        return
+                    }
+                    validUseStat(URL_POOL[i], uid, result)
+                }
+                result(currentUrl)
+            })
+    }
+
 }
 
 
