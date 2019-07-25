e0118f9f78ec4d7a092458b86464f4bbba7551d6
stiartsly <stiartsly@gmail.com>
2019-07-25 15:59:34 +0800

    Make HiveCallback class can be inherited outside module

--- ElastosHiveSDK/Core/HiveCallback.swift
+++ ElastosHiveSDK/Core/HiveCallback.swift
@@ -22,7 +22,7 @@
 
 import Foundation
 
-public class HiveCallback<T> {
+open class HiveCallback<T> {
     public func didSucceed(_ result: T) -> Void {}
     public func runError(_ error: HiveError) -> Void {}
 }
