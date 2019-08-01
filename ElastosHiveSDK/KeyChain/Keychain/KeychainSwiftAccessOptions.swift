d9ac9fea56d6937d645f10d2633504986cccdbfa
liaihong <478701610@qq.com>
2019-08-01 09:29:02 +0800

    Modify the access levels of some classes (#118)

--- ElastosHiveSDK/KeyChain/Keychain/KeychainSwiftAccessOptions.swift
+++ ElastosHiveSDK/KeyChain/Keychain/KeychainSwiftAccessOptions.swift
@@ -5,7 +5,7 @@ import Security
 These options are used to determine when a keychain item should be readable. The default value is AccessibleWhenUnlocked.
 
 */
-public enum KeychainSwiftAccessOptions {
+ enum KeychainSwiftAccessOptions {
   
   /**
   
