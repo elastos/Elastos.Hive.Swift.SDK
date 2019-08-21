d17f8105db76a002a19171993b8144fc3b75f30e
liaihong <478701610@qq.com>
2019-08-21 14:14:47 +0800

    Improve the API docs (#148)

--- ElastosHiveSDK/Core/DirectoryItem.swift
+++ ElastosHiveSDK/Core/DirectoryItem.swift
@@ -22,79 +22,63 @@
 
 import Foundation
 
-/// In order to implement the optional protocol, protocol extension is adopted.
-protocol DirectoryItem {}
+internal protocol DirectoryItem {
+}
 
 extension DirectoryItem{
-    public func createDirectory(withPath: String) -> HivePromise<HiveDirectoryHandle> {
+    func createDirectory(withPath: String) -> HivePromise<HiveDirectoryHandle> {
         return createDirectory(withPath: withPath, handleBy: HiveCallback<HiveDirectoryHandle>())
     }
-    public func createDirectory(withPath: String, handleBy: HiveCallback<HiveDirectoryHandle>)
-        -> HivePromise<HiveDirectoryHandle>{
-        let error = HiveError.failue(des: "Dummy")
-        return HivePromise<HiveDirectoryHandle>(error: error)
+    func createDirectory(withPath: String, handleBy: HiveCallback<HiveDirectoryHandle>) -> HivePromise<HiveDirectoryHandle>{
+        return HivePromise<HiveDirectoryHandle>(error: HiveError.failue(des: "Dummy"))
     }
 
-    public func directoryHandle(atPath: String) -> HivePromise<HiveDirectoryHandle>{
+    func directoryHandle(atPath: String) -> HivePromise<HiveDirectoryHandle>{
         return directoryHandle(atPath: atPath, handleBy: HiveCallback<HiveDirectoryHandle>())
     }
-    public func directoryHandle(atPath: String, handleBy: HiveCallback<HiveDirectoryHandle>)
-        -> HivePromise<HiveDirectoryHandle>{
-        let error = HiveError.failue(des: "Dummy")
-        return HivePromise<HiveDirectoryHandle>(error: error)
+    func directoryHandle(atPath: String, handleBy: HiveCallback<HiveDirectoryHandle>) -> HivePromise<HiveDirectoryHandle>{
+        return HivePromise<HiveDirectoryHandle>(error: HiveError.failue(des: "Dummy"))
     }
 
-    public func createFile(withPath: String) -> HivePromise<HiveFileHandle>{
+    func createFile(withPath: String) -> HivePromise<HiveFileHandle>{
         return createFile(withPath: withPath, handleBy: HiveCallback<HiveFileHandle>())
     }
-    public func createFile(withPath: String, handleBy: HiveCallback<HiveFileHandle>)
-        -> HivePromise<HiveFileHandle>{
-        let error = HiveError.failue(des: "Dummy")
-        return HivePromise<HiveFileHandle>(error: error)
+    func createFile(withPath: String, handleBy: HiveCallback<HiveFileHandle>) -> HivePromise<HiveFileHandle>{
+        return HivePromise<HiveFileHandle>(error: HiveError.failue(des: "Dummy"))
     }
 
-    public func fileHandle(atPath: String) -> HivePromise<HiveFileHandle>{
+    func fileHandle(atPath: String) -> HivePromise<HiveFileHandle>{
         return fileHandle(atPath: atPath, handleBy: HiveCallback<HiveFileHandle>())
     }
-    public func fileHandle(atPath: String, handleBy: HiveCallback<HiveFileHandle>)
-        -> HivePromise<HiveFileHandle>{
-        let error = HiveError.failue(des: "Dummy")
-        return HivePromise<HiveFileHandle>(error: error)
+    func fileHandle(atPath: String, handleBy: HiveCallback<HiveFileHandle>) -> HivePromise<HiveFileHandle>{
+        return HivePromise<HiveFileHandle>(error: HiveError.failue(des: "Dummy"))
     }
 
-    public func createDirectory(withName: String) -> HivePromise<HiveDirectoryHandle>{
+    func createDirectory(withName: String) -> HivePromise<HiveDirectoryHandle>{
         return createDirectory(withName: withName, handleBy: HiveCallback<HiveDirectoryHandle>())
     }
-    public func createDirectory(withName: String, handleBy: HiveCallback<HiveDirectoryHandle>)
-        -> HivePromise<HiveDirectoryHandle>{
-        let error = HiveError.failue(des: "Dummy")
-        return HivePromise<HiveDirectoryHandle>(error: error)
+    func createDirectory(withName: String, handleBy: HiveCallback<HiveDirectoryHandle>) -> HivePromise<HiveDirectoryHandle>{
+        return HivePromise<HiveDirectoryHandle>(error: HiveError.failue(des: "Dummy"))
     }
 
-    public func directoryHandle(atName: String) -> HivePromise<HiveDirectoryHandle>{
+    func directoryHandle(atName: String) -> HivePromise<HiveDirectoryHandle>{
         return directoryHandle(atName: atName, handleBy: HiveCallback<HiveDirectoryHandle>())
     }
-    public func directoryHandle(atName: String, handleBy: HiveCallback<HiveDirectoryHandle>)
-        -> HivePromise<HiveDirectoryHandle>{
-        let error = HiveError.failue(des: "Dummy")
-        return HivePromise<HiveDirectoryHandle>(error: error)
+    func directoryHandle(atName: String, handleBy: HiveCallback<HiveDirectoryHandle>) -> HivePromise<HiveDirectoryHandle>{
+        return HivePromise<HiveDirectoryHandle>(error: HiveError.failue(des: "Dummy"))
     }
 
-    public func createFile(withName: String) -> HivePromise<HiveFileHandle>{
+    func createFile(withName: String) -> HivePromise<HiveFileHandle>{
         return createFile(withName: withName, handleBy: HiveCallback<HiveFileHandle>())
     }
-    public func createFile(withName: String, handleBy: HiveCallback<HiveFileHandle>)
-        -> HivePromise<HiveFileHandle>{
-        let error = HiveError.failue(des: "Dummy")
-        return HivePromise<HiveFileHandle>(error: error)
+    func createFile(withName: String, handleBy: HiveCallback<HiveFileHandle>) -> HivePromise<HiveFileHandle>{
+        return HivePromise<HiveFileHandle>(error: HiveError.failue(des: "Dummy"))
     }
 
-    public func fileHandle(atName: String) -> HivePromise<HiveFileHandle>{
+    func fileHandle(atName: String) -> HivePromise<HiveFileHandle>{
         return fileHandle(atName: atName, handleBy: HiveCallback<HiveFileHandle>())
     }
-    public func fileHandle(atName: String, handleBy: HiveCallback<HiveFileHandle>)
-        -> HivePromise<HiveFileHandle>{
-        let error = HiveError.failue(des: "Dummy")
-        return HivePromise<HiveFileHandle>(error: error)
+    func fileHandle(atName: String, handleBy: HiveCallback<HiveFileHandle>) -> HivePromise<HiveFileHandle>{
+        return HivePromise<HiveFileHandle>(error: HiveError.failue(des: "Dummy"))
     }
 }
