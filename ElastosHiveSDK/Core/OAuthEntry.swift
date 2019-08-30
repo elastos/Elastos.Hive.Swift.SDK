b1aada7aeb3720882638ca576f47dd2933200d9a
stiartsly <stiartsly@gmail.com>
2019-08-30 12:09:08 +0800

    Improve API docs (#152)

--- ElastosHiveSDK/Core/OAuthEntry.swift
+++ ElastosHiveSDK/Core/OAuthEntry.swift
@@ -22,30 +22,24 @@
 
 import Foundation
 
-/// Configuration for the ElastosHiveSDK.
-/// All app information should match the information in your app portal with Microsoft Accounts or Azure Active Directory.
-/// @see https://dev.onedrive.com/README.htm for more information.
+/// An entry includes all necessary information to get authorization from
+/// remote OAuth2 server.
 public class OAuthEntry {
 
-    /// ClientId for OneDrive registered with Microsoft Account.
-    /// @see https://dev.onedrive.com/auth/msa_oauth.htm for registration information.
+    /// The clientId that registered 3rd-party OAuth2 server.
     let clientId: String
 
-    /// Scopes to be used for OneDrive registered with Microsoft Account.
-    /// @see https://dev.onedrive.com/auth/msa_oauth.htm for registration information.
-    let scope   : String
+    /// The scope of permissions
+    let scope: String
 
-    /// Redirect URL for OneDrive.
-    /// @see https://dev.onedrive.com/auth/aad_oauth.htm for registration information.
-    /// @warning This value must be the same as the RedirectURL provided in the Azure Active Directory portal.
+    /// Redirect URL
     let redirectURL: String
 
-    /// Create OAuthEntry instance
+    /// An constructor
     ///
-    /// - Parameters:
-    ///   - clientId: The clientId
-    ///   - scope: The scope
-    ///   - redirectURL: The redirectURL
+    /// - Parameter clientId: the registered clientId
+    /// - Parameter scope: The registered scope of permissions
+    /// - Parameter redirectURL: the redirect URL.
     public init(_ clientId: String,  _ scope: String, _ redirectURL: String) {
         self.clientId = clientId
         self.scope = scope
