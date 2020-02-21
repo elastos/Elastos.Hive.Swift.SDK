0c8a6893b266611af2c70634e2033ee05b7166b5
liaihong <478701610@qq.com>
2020-02-21 11:41:39 +0800

    implement AuthInfoStoreImpl apis

--- ElastosHiveSDK/Vendors/OneDrive/AuthInfoStoreImpl.swift
+++ ElastosHiveSDK/Vendors/OneDrive/AuthInfoStoreImpl.swift
@@ -6,7 +6,19 @@ class AuthInfoStoreImpl: Persistent {
     }
     
     func parseFrom() -> Dictionary<String, Any> {
-        
+        let fileManager = FileManager.default
+        let exist: Bool = fileManager.fileExists(atPath: path)
+        if (exist) {
+            do {
+                let readHandler = FileHandle(forReadingAtPath: path)!
+                let data = readHandler.readDataToEndOfFile()
+                let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? [String : Any] ?? [: ]
+                return json
+            }
+            catch {
+                print(error)
+            }
+        }
         return [: ]
     }
     
@@ -16,6 +28,11 @@ class AuthInfoStoreImpl: Persistent {
         if !exist {
             fileManager.createFile(atPath: path, contents: nil, attributes: nil)
         }
-        //todo: seve
+        do {
+            let data = try JSONSerialization.data(withJSONObject: json, options: [])
+            try data.write(to: URL(fileURLWithPath: path))
+        } catch {
+            print(error)
+        }
     }
 }
