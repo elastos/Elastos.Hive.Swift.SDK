c7b82ec5d52f2abd3ae42b6d0d220fc4d937d110
liaihong <478701610@qq.com>
2020-03-05 16:36:36 +0800

    rename sizeofRemoteFile(fileName: String) to sizeofRemoteFile(fileName: Hash) (#173)

--- ElastosHiveSDK/Protocols/IPFSProtocol.swift
+++ ElastosHiveSDK/Protocols/IPFSProtocol.swift
@@ -15,8 +15,8 @@ public protocol IPFSProtocol {
     func putDataFromInputStream(_ input: InputStream) -> HivePromise<Hash>
     func putDataFromInputStream(_ input: InputStream, handler: HiveCallback<Hash>) -> HivePromise<Hash>
 
-    func sizeofRemoteFile(_ fileName: String) -> HivePromise<UInt64>
-    func sizeofRemoteFile(_ fileName: String, handler: HiveCallback<UInt64>) -> HivePromise<UInt64>
+    func sizeofRemoteFile(_ fileName: Hash) -> HivePromise<UInt64>
+    func sizeofRemoteFile(_ fileName: Hash, handler: HiveCallback<UInt64>) -> HivePromise<UInt64>
 
     func getString(fromRemoteFile: Hash) -> HivePromise<String>
     func getString(fromRemoteFile: Hash, handler: HiveCallback<String>) -> HivePromise<String>
