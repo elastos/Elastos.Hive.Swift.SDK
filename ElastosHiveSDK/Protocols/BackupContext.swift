b141a195b979223b8f368825603d1295eec79da1
hancongyang <unknown>
2021-03-16 10:17:46 +0800

    add swift protocol like java Service and  new managing URLs and endpoints way

--- ElastosHiveSDK/Protocols/BackupContext.swift
+++ ElastosHiveSDK/Protocols/BackupContext.swift
@@ -22,37 +22,10 @@
 
 import Foundation
 
-public protocol PaymentProtocol {
-
-    /// Get vault's payment info
-    /// PricingPlan list
-    func getPaymentInfo() -> Promise<PricingInfo>
-
-    /// Get vault pricing plan information by plan name
-    /// - Parameter planName: planName plan name
-    /// Return: the instance of PricingPlan
-    func getPricingPlan(_ planName: String) -> Promise<PricingPlan>
+public protocol BackupContext {
+    var type: String { get set }
     
-    /// Get payment version
-    func getPaymentVersion() -> Promise<String>
+    var parameter: String { get set }
 
-    /// Create a order of pricing plan
-    /// - Parameter priceName: priceName
-    func placeOrder(_ priceName: String) -> Promise<String>
-
-    /// Pay for  pricing plan order
-    /// - Parameters:
-    ///   - orderId: orderId
-    ///   - txids: txids
-    func payOrder(_ orderId: String, _ txids: Array<String>) -> Promise<Bool>
-
-    /// Get order information of vault service purchase
-    /// - Parameter orderId: orderId
-    func getOrder(_ orderId: String) -> Promise<Order>
-
-    /// Get user order information list of vault service purchase
-    func getAllOrders() -> Promise<Array<Order>>
-    
-    /// Get using price plan
-    func getUsingPricePlan() -> Promise<UsingPlan>
+    func getAuthorization(_ request: String) -> Promise<String>    
 }
