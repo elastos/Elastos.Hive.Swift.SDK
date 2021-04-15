87fc3354c6bc8b7dbbdc6f101b08c84376036766
HanCongYang <495949598@qq.com>
2021-04-15 14:55:53 +0800

    1. add error type

--- ElastosHiveSDK/Exception/HiveError.swift
+++ ElastosHiveSDK/Exception/HiveError.swift
@@ -39,8 +39,16 @@ public enum HiveError: Error {
     case ProviderNotSetException(_ message: String)
     case VaultAlreadyExistException(_ message: String)
     case UnsupportedOperationException
-
-    
+    case BackupAlreadyExistException(_ message: String)
+    case FileDoesNotExistsException
+    case HiveSdkException(_ message: String)
+    case HttpFailedException(_ message: String)
+    case IllegalDidFormatException(_ message: String)
+    case InvalidParameterException(_ message: String)
+    case NoEnoughSpaceException(_ message: String)
+    case UnauthorizedStateException(_ message: String)
+    case UnsupportedFileTypeException(_ message: String)
+    case VaultLockedException(_ message: String)
 }
 
 extension HiveError {
@@ -63,8 +71,6 @@ extension HiveError {
             return des ?? ""
         case .hiveSdk(let message):
             return message
-        case .unknownBackupState(let message):
-            return message
         case .failedToGetBackupState:
             return "Failed to get back-up state."
         case .DIDResoverAlreadySetupException:
@@ -81,7 +87,26 @@ extension HiveError {
             return "VaultAlreadyExistException: \(message)"
         case .UnsupportedOperationException:
             return "unsupported operation exception"
-
+        case .BackupAlreadyExistException(let message):
+            return "BackupAlreadyExistException: \(message)"
+        case .FileDoesNotExistsException:
+            return "FileDoesNotExistsException"
+        case .UnsupportedFileTypeException:
+            return "Unsupport file type"
+        case .HiveSdkException(let message):
+            return "HiveSdkException \(message)"
+        case .HttpFailedException(let message):
+            return "HttpFailedException \(message)"
+        case .IllegalDidFormatException(let message):
+            return "IllegalDidFormatException \(message)"
+        case .InvalidParameterException(let message):
+            return "InvalidParameterException \(message)"
+        case .NoEnoughSpaceException(let message):
+            return "NoEnoughSpaceException \(message)"
+        case .UnauthorizedStateException(let message):
+            return "UnauthorizedStateException \(message)"
+        case .VaultLockedException(let message):
+            return "VaultLockedException \(message)"
         }
     }
     
